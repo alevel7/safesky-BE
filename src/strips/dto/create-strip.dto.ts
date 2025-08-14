2a82e7fe57bf065127a439a9c56ce953ca5a3bc5
Taiwo kazeem <kazeem@kobo360.com>
2025-08-14 10:58:49 +0100

    feat: add payload validation for update and create strip

--- src/strips/dto/create-strip.dto.ts
+++ src/strips/dto/create-strip.dto.ts
@@ -1,1 +1,60 @@
-export class CreateStripDto {}
+import { IsNotEmpty } from "class-validator";
+
+export class CreateStripDto {
+
+    @IsNotEmpty()
+    callsign: string;
+
+    @IsNotEmpty()
+    aircraftType: string;
+    
+    @IsNotEmpty()
+    departureAerodrome: string;
+
+    plannedRoute: string;
+
+    speed: string;
+
+    @IsNotEmpty()
+    arrivalAerodrome: string;
+
+    runway: string;
+
+    clearanceType: string; //I,V, Y, Z
+
+    squawkCode: string;
+
+    isArchived: boolean;
+
+    estimatedTimeOver: string;
+
+    actualTimeOver: string;
+
+    timeAtTransfer: string; //Reported time at transfer
+
+    actualTimeLeavingHold: string; //ALTH
+
+    clearedFlightLevel: string;
+
+    altitude: string;
+
+    heading: string;
+
+    estimatedTimeOfArrival: string;
+
+    actualTimeOfArrival: string;
+
+    expectedApproachTime: string;
+
+    frequency: string;
+
+    stripType: string;
+
+    stripStatus: string;
+
+    passengerOnBoard: number;
+
+    clearedRoute: number;
+
+    wakeTurbulence: string; //H, L, M, J)
+}
